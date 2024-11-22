03c98180e843abb7576140179e346d4f6cdd34e7
Tiago Altomare <tiagoa@ciandt.com>
2024-11-22 09:21:17 -0300

    Atualização dos enunciados

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -18,7 +18,7 @@ export class AppController {
   ) {}
 
   @Get('desafio-um')
-  @ApiOperation({ description: 'O time da TechTaco começou a fazer a funcionalidade de autenticação, mas não conseguiu terminar. Finalizar este ponto seria o primeiro passo a ser feito no escopo do projeto. A saída esperada para essa API é uma string com token de autenticação' })
+  @ApiOperation({ description: 'O time da TechTaco começou a fazer a funcionalidade de autenticação de professores e alunos, mas não conseguiu terminar. Finalizar este ponto seria o primeiro passo a ser feito no escopo do projeto. A saída esperada para essa API é uma string com token de autenticação' })
   async getDesafioUm(): Promise<any> {
     // return await this.desafio1Service.execute();
   }
@@ -30,19 +30,19 @@ export class AppController {
   }
 
   @Get('desafio-tres')
-  @ApiOperation({ description: 'O TalentTAP substitui um sistema legado da MentorMunch que já possuía uma lista de empresas que foi migrada para o TalentTAP. Não existe garantia da consistência e validade dos dados dessas empresas, por isso será necessária uma verificação dos CNPJs dessa lista. Precisamos atualizar os CNPJs inconsistentes em nossa base utilizando a api do desafio 2 e atualizando os dados conforme o cadastro no Governo.' })
+  @ApiOperation({ description: 'O TalentTAP substitui um sistema legado da MentorMunch que já possuía uma lista de empresas que foram migradas para o TalentTAP. Não existe garantia da consistência e validade dos dados dessas empresas, por isso será necessária uma verificação dos CNPJs pré existentes e atualizar-los em nossa base. Considerem utilizar a api do desafio 2 para validar os dados e as APIS do sefaz para buscar os dados corretos para fazer as atualizações necessárias.' })
   async getDesafioTres(): Promise<any> {
     // return await this.desafio3Service.execute();
   }
 
   @Get('desafio-quatro')
-  @ApiOperation({ description: 'Um dos maiores desejos do PO do projeto é uma funcionalidade que consiga acelerar a filtragem de candidatos para possíveis oportunidades e contratações. Esse filtro deve funcionar as seguintes premissas/regras de negócio: \n1. Eu como empresa preciso que essa lista contenha apenas mentorados que possuam as skills que minha empresa tem interesse atualmente.\n2. Quero incluir nessa lista apenas os talentos que sejam mentorados pelos 10 melhores Mentores da minha empresa.\n\n\t- Caso minha empresa tenho menos que 10 mentores naquelas skills, considerar mentores de outras empresas também.\n3. Para essa lista, quero ver a lista de talentos, com os feedbacks relativos aquelas skills que minha empresa tem interesse.\n\n\t- Existe um serviço que retorna, a partir de um aluno, uma skill e a avaliação atual daquele aluno, um racional do porquê daquela avaliação.' })
+  @ApiOperation({ description: 'Um dos maiores desejos do PO do projeto é uma funcionalidade que consiga acelerar a filtragem de candidatos para possíveis oportunidades e contratações. Esse filtro deve funcionar as seguintes premissas/regras de negócio: \n1. Eu como empresa preciso de um relatório que contenha apenas alunos que possuam talentos que minha empresa tem interesse atualmente.\n2. Quero incluir nesse relatório apenas os talentos que sejam ensinados pelos 10 melhores professores que trabalhem em minha empresa.\n\n\t- Caso minha empresa tenho menos que 10 professores naqueles talentos, considerar professores de outras empresas também.\n3. Para esse relatório, quero ver a lista de talentos com seus respectivos feedbacks relativos a elas e que minha empresa tem interesse.\n\n\t- Existe um serviço que retorna a partir de um aluno, um talento e a avaliação atual daquele aluno, um racional do porquê daquela avaliação.' })
   async getDesafioQuatro(): Promise<any> {
     // return await this.desafio4Service.execute();
   }
 
   @Post('desafio-cinco')
-  @ApiOperation({ description: 'A partir do racional retornado pelo serviço de acima, gostaríamos de utilizar a IA para nos retornar, para aquele candidato:\n1. Uma classificação do resultado do candidato entre as seguintes categorias:\n\n\t- Pode melhorar\n\n\t- Atende as Expectativas\n\n\t- Bom resultado\n\n\t- Excelente resultado\n2. Veredito para contratação\n\n\t- Esse resultado deve dizer se o candidato deve ser contratado ou não, e o porquê para tal decisão.' })
+  @ApiOperation({ description: 'A partir do retorno do desafio 4, gostaríamos de utilizar a IA para nos retornar para um aluno:\n1. Uma classificação do resultado do aluno entre as seguintes categorias:\n\n\t- Pode melhorar\n\n\t- Atende as Expectativas\n\n\t- Bom resultado\n\n\t- Excelente resultado\n2. Veredito para contratação\n\n\t- Esse resultado deve dizer se o aluno deve ser contratado ou não, e o porquê para tal decisão.' })
   async getDesafioCinco(
   ): Promise<any> {
     // return await this.desafio5Service.execute();
